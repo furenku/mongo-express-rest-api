ac6a15be07d0613516aaa031d60bd98b409c0baf
furenku <furenku@gmail.com>
2018-11-22 19:07:20 -0600

    fixed bug in model export

--- models/task-model.js
+++ models/task-model.js
@@ -4,4 +4,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/task_db')
 
 var TaskSchema = require('./schemas').TaskSchema
 
-module.exports = mongoose.model('instruction', TaskSchema)
+module.exports = mongoose.model('task', TaskSchema)
